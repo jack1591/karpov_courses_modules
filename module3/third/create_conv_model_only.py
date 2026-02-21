cfd73148e6c15a427294b2d3c80d9f75882beda6
Artem <jack.tag@mail.ru>
2026-02-21 12:52:30 +0300

    convolutional nn added

---
+++ module3/third/create_conv_model_only.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import torch
+from torch import nn
+def create_conv_model():
+    return nn.Sequential(
+        nn.Conv2d(in_channels = 1, out_channels = 32, kernel_size = 5),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size = 2),
+
+        nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 5),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size = 2),
+
+        nn.Flatten(),
+        nn.Linear(4 * 4 * 64, 256),
+        nn.ReLU(),
+        nn.Linear(256, 10)
+    )
+
